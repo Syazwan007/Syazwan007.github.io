1107b5e8f8a85ec4e31b96012a5a01852a96d16a
Syazwan Haziq <syazwanhaziq793@gmail.com>
2025-04-14 16:55:06 +0800

    Add files via upload

--- function.js
+++ function.js
@@ -44,3 +44,38 @@ function orderFood() {
     info.innerText = checked_values.join(', ');
     priceinfo.innerHTML = "<strong>Total Price : RM " + price +"\n<strong>"
 }
+
+let btnKlik = document.getElementById("Klik");
+
+btnKlik.onclick = function() {
+    alert('Button ditekan');
+}
+let txtWelcome = document.getElementById('welcome');
+
+txtWelcome.onmouseover = function() {
+    txtWelcome.innerText= "Selamat Datang"
+}
+txtWelcome.onmouseout = function() {
+    txtWelcome.innerText = "Welcome"
+}
+
+let peter = document.getElementById('peter');
+peter.onmouseover = function() {
+    this.src="https://static.wikia.nocookie.net/schedule-1/images/f/f9/Sam_Mugshot.png/revision/latest/thumbnail/width/360/height/450?cb=20250406110401"
+    this.style.filter = "blur(0px)";
+    this.style.transform ="scale(1.5)"
+    this.style.transition ="transform 0.5s ease-in-out"
+}
+peter.onmouseout = function() {
+    this.src="https://static.wikia.nocookie.net/schedule-1/images/c/c1/Peter_Mugshot.png/revision/latest?cb=20250406110350"
+    this.style.filter = "blur(2px)";
+    this.style.transform ="scale(1)"
+}
+    function checkJabatan(){
+        let jabatan =document.getElementById('jabatan')
+        let selectedJabatan = jabatan.options[jabatan.selectedIndex].value;
+
+        alert("You have selected : "+selectedJabatan);
+        console.log(selectedJabatan);
+    }
+
